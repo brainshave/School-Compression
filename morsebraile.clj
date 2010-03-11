8c3c6358e4b4d067a3c0666357f79d8ab7c894ba
Szymon Witamborski <santamon@gmail.com>
2010-03-11 19:12:29 +0100

    asdf

--- morsebraile.clj
+++ morsebraile.clj
@@ -3,7 +3,7 @@
   morsebraile
   (:require (clojure.contrib [string :as ccs])))
 
-(def #^{:doc "Sorted map with Morse codes"
+(def #^{:doc "Map with Morse codes"
 	:test (fn [] (map #(when (not (= (last %) \space)) %) (vals morse)))}
      morse-data {\a ".- "	    \b "-... "	    \c "-.-. "
 		 \d "-.. "	    \e ". "	    \f "..-. "	    \g "--. "
@@ -20,18 +20,22 @@
 		 \' ".----. "       \_ "..--.- "    \: "---... "    \? "..--.. "
 		 \- "-....- "       \/ "-..-. "	    \( "-.--. "	    \) "-.--.- "
 		 \= "-...- "	    \@ ".--.-. " \space \space \newline \newline})
+
+(def #^{:doc "Map with Braile codes"}
+     braile-data {}) 
+
 (def morse-data-inv
      (apply hash-map (interleave (map str (vals morse-data)) (keys morse-data))))
 
 (defn morse
-  "Converts to morse code using morse-data as a refernce and checking for 'ch'"
+  "Converts to morse code using morse-data as a reference and checking for 'ch'"
   [s]
   (let [ss (conj (apply vector \0 (.toLowerCase s)) \0)] ; add \0 to the end and begining
     (apply str (map #(cond (and (= %2 \c) (= %3 \h)) "---- " ;; ch
 			   (and (not= %1 \c) (not= %2 \h)) (morse-data %2))
 		   ss (next ss) (nnext ss)))))
 (defn unmorse
-  ""
+  "Decodes Morse code"
   [s]
   (apply str (map morse-data-inv (ccs/partition #"[.-]+\s" s))))
   
