88c1b78a7c9319fa5e1753af892823919d23b24b
Szymon Witamborski <santamon@gmail.com>
2010-03-25 18:43:27 +0100

    ambiguous codes working

--- ambcodes.clj
+++ ambcodes.clj
@@ -5,7 +5,8 @@
 (defn starts-with
   "Returns true if sequence a starts with b"
   [a b]
-  (= (take (count b) a) (seq b)))
+  (= (take (count b) a)
+     b))
 
 (defn suffix
   [a b]
@@ -15,28 +16,35 @@
       (let [f #(if (starts-with %1 %2) (drop (count %2) %1))]
 	(or (f a b) (f b a))))))
 
+(defn filter-not-nil [coll]
+  (filter #(not (nil? %)) coll))
+
 (defn find-suffixes
   ([code suffixes]
      (let [words (for [c code s suffixes]
 		   (suffix c s))]
        (if (not-any? #(= :same-words %) words)
-	 (into #{} words)
+	 (into #{} (filter-not-nil words))
 	 :same-words)))
-  ([code] (into #{} (filter #(not (nil? %))
-			    (for [c1 code c2 code :when (not= c1 c2)]
-			      (suffix c1 c2))))))
+  ([code] (into #{} (filter-not-nil
+		     (for [c1 code c2 code :when (not= c1 c2)]
+		       (suffix c1 c2))))))
 
 (defn unambiguous?
   "Is given code unambiguous?"
   [code]
-;  {:pre [(not-any? true? (for [c1 code c2 code]
-;			   (= c1 c2)))]}
-  (loop [suffixes (find-suffixes code)]
-    (let [new-suffixes (find-suffixes code suffixes)]
-      (cond (= :same-words new-suffixes) false
-	    (every? new-suffixes suffixes) true
-	    (empty? new-suffixes) true
-	    true (recur (into suffixes new-suffixes))))))
+  (if-not (apply distinct? code)
+    false
+    (loop [suffixes (find-suffixes code)]
+      (let [new-suffixes (find-suffixes code suffixes)]
+	;(println suffixes \tab new-suffixes)
+	(cond (= :same-words new-suffixes) false
+	      (every? suffixes new-suffixes) true
+	      (empty? new-suffixes) true
+	      true (recur (into suffixes new-suffixes)))))))
+
+
+  
 	    
     
     
