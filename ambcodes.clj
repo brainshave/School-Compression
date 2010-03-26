fd69cc9ccfea9a37044f98784093fbc156db1875
Szymon Witamborski <santamon@gmail.com>
2010-03-26 17:18:47 +0100

    stupid-generator

--- ambcodes.clj
+++ ambcodes.clj
@@ -47,16 +47,13 @@ when two words have suffix that is a word in code"
       (loop [suffixes #{}
 	     candidates (find-suffixes code)]
 	(let [amb (first (filter vector? candidates))]
-	  (println suffixes \tab candidates \tab amb)
+	  ;(println suffixes \tab candidates \tab amb)
 	  (cond amb amb ;; return first ambiguity
 		(every? suffixes candidates) false
 		(empty? candidates) false
 		:default (let [new-suffixes (into suffixes candidates)]
 			   (recur new-suffixes
 				  (find-suffixes code new-suffixes)))))))))
-      
-      
-
 
 (defn rand-word [max-len]
   (take (inc (rand-int max-len))
@@ -66,6 +63,21 @@ when two words have suffix that is a word in code"
   (if (== (first word) 0)
     (conj (rest word) 1)
     (conj word 0)))
+
+(defn rand-alphabet [n max-len]
+  (repeatedly n #(rand-word max-len)))
+
+(defn stupid-generator
+  "Stupid generator, just generating random alphabets of n length with
+words of max-len length.
+Stops after max-tries"
+  ([n max-len max-tries]
+     (loop [alphabet (rand-alphabet n max-len)
+	    tries (int 0)]
+       (cond (== (inc tries) max-tries) nil
+	     (ambiguous? alphabet) (recur (rand-alphabet n max-len) (inc tries))
+	     :default (with-meta alphabet {:tries tries}))))
+  ([n] (stupid-generator n (int (* n 3/4)) (* n n n))))
 
 (defn new-alphabet2 [n]
   (loop [code #{} word (next-word ())]
